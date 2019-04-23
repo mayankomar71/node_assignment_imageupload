5e02c5172c78e694ceb9d16e3da5b7fa2912e02d
Mayank Omar <mayankomar71@gmail.com>
2019-04-23 18:03:08 +0530

    uploading using base64

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -32,6 +32,38 @@ exports.postImage = (req, res) => {
     return req.pipe(busboy);
 
 }
+exports.base64upload=function(req, res){
+    var base64Data = req.body.image.replace(/^data:image\/(?:jpeg|jpg|JPEG|JPG|png|PNG);base64,/, "");
+    var filename = RandomString.generate(7)
+    let extension, lowerCaseData = base64Data.toLowerCase();
+    if(lowerCaseData.indexOf('png') !== -1){
+        extension = '.png'
+    }else if(lowerCaseData.indexOf('jpg') !== -1){
+        extension = '.jpg'
+    }else if(lowerCaseData.indexOf('jpeg') !== -1){
+        extension = '.jpeg'
+    }
+    fs.writeFile('./temp/' + filename + extension, base64Data, 'base64', function (err) {
+        
+        const data = new image({
+            url:'/temp/' + filename + extension,
+            fileName:filename
+     
+         })   
+                
+      
+
+        data.save(function(err){
+            if(err){
+                res.json(err);
+            }
+        else
+        {
+            res.json('file uploaded using base64')
+        }
+        });
+    });
+}
 // exports.findImage=(req,res)=>
 // {
 //     image.find({'url':req.params.url}).then((response)=>
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,5 +1,5 @@
 module.exports=(app)=>{
     const imageupload=require('../controllers/controllers.js');
     app.post('/upload', imageupload.postImage);
-    app.get('/image/:url',imageupload.findImage)
+    app.post('/base64', imageupload.base64upload);
 }
